f1f7d8489134d8a540c74fb19e0c17cfb7ac29be
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2023-01-26 22:15:44 +0530

    Added Mood Analyse Happy or Sad Program

---
+++ MoodAnalyserProblem/MoodAnalyser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserProblem
+{
+    public  class MoodAnalyser
+    {
+        public const string Happy_Mood = "Happy";
+        public const string Sad_Mood = "Sad";
+        public string Mood(string mood)
+        {
+            if (mood.ToLower().Contains("happy"))
+            {
+                Console.WriteLine(Happy_Mood);
+                return mood;
+            }
+            if (mood.ToLower().Contains("sad"))
+            {
+                Console.WriteLine(Sad_Mood);
+                return mood;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
+
--- MoodAnalyserTest/UnitTest1.cs
+++ MoodAnalyserTest/UnitTest1.cs
@@ -1,11 +1,20 @@
+using MoodAnalyserProblem;
+
 namespace MoodAnalyserTest
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+
+        public void GiveMoodShouldReturnSadMood()
         {
+            const string happymood = "Happy";
+            const string sadmood = "Sad";
+            MoodAnalyser mood = new MoodAnalyser();
+            string AcutalResult = mood.Mood(sadmood);
+            string expected = "Sad";
+            Assert.AreEqual(expected, AcutalResult);
         }
     }
 }
